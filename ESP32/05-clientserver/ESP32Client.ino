e8d8098ee8c24e4b5d2e3b2d7175991eb23d10f1
TarunBharani <tarun374374@gmail.com>
2022-06-18 11:58:06 +0530

    Update ESP32Client.ino

--- ESP32/05-clientserver/ESP32Client.ino
+++ ESP32/05-clientserver/ESP32Client.ino
@@ -1,8 +1,7 @@
-/////////////////////////////////
-// Generated with a lot of love//
-// with TUNIOT FOR ESP32     //
-// Website: Easycoding.tn      //
-/////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+// Please use Your Mobile HotSpot to Test This Program                          //
+// Also Note that CDAC Network has Jammers that Block the HOTSPOT of your Mobile//
+////////////////////////////////////////////////
 #include <WiFi.h>
 
 WiFiClient client;
@@ -15,23 +14,27 @@ Serial.begin(9600);
   WiFi.disconnect();
   delay(3000);
   Serial.println("START");
-  WiFi.begin("my-net","");
+  WiFi.begin("Your_SSID_Name","Password");
   while ((!(WiFi.status() == WL_CONNECTED))){
     delay(300);
     Serial.print("..");
 
   }
-  Serial.println("Connected");
+  Serial.println("Connected to HotSpot");
   Serial.println("Your IP is");
   Serial.println((WiFi.localIP()));
-
+  Serial.println("Note the IP Address and Find the Network IP");
+  Serial.println("Configure the same Network IP in Server");
 }
 
 
 void loop()
 {
 
-    if (client.connect("192.168.1.10", 80)) {
+    if (client.connect("192.168.1.10", 80)) 
+	{ 
+	  Serial.println("OK :Connected To Server");
+	  Serial.println("Sending Data to Server");
       myurl += "yes";
       client.print(String("GET ") + myurl + " HTTP/1.1\r\n" +
                 "Host: " + "192.168.1.10" + "\r\n" +
@@ -39,24 +42,13 @@ void loop()
       myurl="/";
 
     }
+	else 
+	{
+		Serial.println("NOK : Unable Connect Server");
+	}
+	
     delay(1000);
-    if (client.connect("192.168.1.10", 80)) {
-      myurl += "yes";
-      client.print(String("GET ") + myurl + " HTTP/1.1\r\n" +
-                "Host: " + "192.168.1.10" + "\r\n" +
-               "Connection: close\r\n\r\n");
-      myurl="/";
-
-    }
-    delay(1000);
-    if (client.connect("192.168.1.10", 80)) {
-      myurl += "yes";
-      client.print(String("GET ") + myurl + " HTTP/1.1\r\n" +
-                "Host: " + "192.168.1.10" + "\r\n" +
-               "Connection: close\r\n\r\n");
-      myurl="/";
-
-    }
-    delay(1000);
+    
+    
 
 }
